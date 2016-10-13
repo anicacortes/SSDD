a62c812aced3a8fa4da158a06284ab0fa7ebab27
beapc18 <beapc_18@hotmail.com>
2016-10-14 00:28:54 +0200

    solucionados problemas get, falta post

--- src/ssdd/p1/ServidorRunnable.java
+++ src/ssdd/p1/ServidorRunnable.java
@@ -3,8 +3,6 @@ package ssdd.p1;
 import java.io.*;
 import java.net.Socket;
 import java.net.URLDecoder;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,19 +11,19 @@ public class ServidorRunnable implements Runnable{
 	// Guardar direccion y puerto donde escucha servidor
 	static private String ADDRESS;
 	static private int PORT;
-    private Socket clientSocket; //socket de conexion con cliente
-    private OutputStream clientResponse;
+    static private Socket clientSocket; //socket de conexion con cliente
+    static private OutputStream clientResponse;
 
     BlockingHTTPParser parser = new BlockingHTTPParser();
     //Info de respuesta del servidor
-	private String estado = "HTTP/1.1 ";
-	private String path;
-	private String body;
-    private String type;
-    private String length = "Content-Length: ";
-	private URLDecoder dec;
-    private Pattern fname = Pattern.compile("fname=.*&");
-    private Pattern content = Pattern.compile("&content=.*");
+    static private String estado;
+    static private String path;
+    static private String body;
+    static private String type;
+    static private String length;
+    static private URLDecoder dec;
+    static private Pattern fname = Pattern.compile("fname=.*&");
+    static private Pattern content = Pattern.compile("&content=.*");
 
     private static String FORBIDDEN = "<html><head>\n<title>403 Forbidden</title>\n" +
             "</head><body>\n<h1>Forbidden</h1>\n</body></html>\n";
@@ -44,27 +42,19 @@ public class ServidorRunnable implements Runnable{
 			parser.parseRequest(clientSocket.getInputStream());
 			//Peticion incompleta
 			if(!parser.isComplete()){
-				estado += "400 Bad Request\n";
+				estado = "HTTP/1.1 400 Bad Request\n";
 			}
 			//Peticion tipo GET
 			else {
 				if (parser.getMethod().equalsIgnoreCase("GET")){
 					path = parser.getPath();
-					estado += buscarFichero(path);
-
-                    //Devuelve contenido al cliente por bytebuffer
-                    //
-                    Integer aux = body.length();
-                    length += aux.toString()+"\n\n";
-                    clientResponse = clientSocket.getOutputStream();
-                    clientResponse.write(estado.getBytes());
-                    clientResponse.write(length.getBytes());
-                    clientResponse.write(body.getBytes());
-                    clientResponse.close();
+					estado = buscarFichero(path);
+                    respuestaCliente();
 				}
                 //Peticion tipo POST
 				else if (parser.getMethod().equalsIgnoreCase("POST")){
 					body = new String(parser.getBody().array(), "UTF-8");
+                    System.out.println("body recibido: " + body);
 					dec = new URLDecoder();
 					String descodificado = dec.decode(body, "UTF-8");
 
@@ -73,16 +63,17 @@ public class ServidorRunnable implements Runnable{
                     if (matcherF.matches()) {
                         path = matcherF.group();
                     }
+                    System.out.println("path: "+path);
                     //Comprobaciones del fichero
                     estado = buscarFichero(path);
                     if (matcherC.matches()) {
                         body = matcherC.group();
                     }
-
-
+                    System.out.println("body: "+body);
+                    respuestaCliente();
 				} else {
                     //Metodo no aceptado
-					estado += "501 Not Implemented\n";
+					estado = "HTTP/1.1 501 Not Implemented\n";
 				}
 			}
 		}catch (IOException e) {
@@ -90,7 +81,7 @@ public class ServidorRunnable implements Runnable{
    			System.exit(-1);
    		}
    		System.out.println(estado);
-        System.out.println(type);
+        System.out.println(path);
         System.out.println(length);
         System.out.println(body);
 	}
@@ -103,20 +94,20 @@ public class ServidorRunnable implements Runnable{
 		String[] subDirs = path.split("/");
 		if(subDirs.length>2){
             body = FORBIDDEN;
-			return "403 Forbidden\n";
+			return "HTTP/1.1 403 Forbidden\n";
 		}else if(subDirs.length == 0) {
             body = NOTFOUND;
-            return "404 Not Found\n";
+            return "HTTP/1.1 404 Not Found\n";
         }else{
             //obtiene path actual
             String p = System.getProperty("user.dir") + path;
             File dir = new File(p);
             if (dir.exists()) {
                 body = leerFichero(p);
-                return "200 OK\n";
+                return "HTTP/1.1 200 OK\n";
             } else {
                 body = NOTFOUND;
-                return "404 Not Found\n";
+                return "HTTP/1.1 404 Not Found\n";
             }
         }
 	}
@@ -139,5 +130,16 @@ public class ServidorRunnable implements Runnable{
         catch (FileNotFoundException e){System.out.println("No se ha encontrado el fichero");}
         catch (IOException e){System.out.println("Excepcion: "+e);}
         return null;
+    }
+
+    private static void respuestaCliente() throws IOException{
+        //Devuelve contenido al cliente
+        Integer aux = body.length();
+        length = "Content-Length: " + aux.toString() + "\n\n";
+        clientResponse = clientSocket.getOutputStream();
+        clientResponse.write(estado.getBytes());
+        clientResponse.write(length.getBytes());
+        clientResponse.write(body.getBytes());
+        clientResponse.close();
     }
 }
